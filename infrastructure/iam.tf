e9c33835eded864f6a2721ed290dbea7aed7e75e
psegynola <philipsegynola@gmail.com>
2026-08-06 16:27:19 -0600

    fixed creating the policy document

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -42,6 +42,7 @@ data "aws_iam_policy_document" "tf_backend" {
   }
 }
 
+#This creates the document
 resource "aws_iam_policy" "tf_backend" {
   name        = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
   description = "Allow user to use S3 and DynamoDB for TF backend resources"
