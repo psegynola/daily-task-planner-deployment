2163bfa6ba89d75bbacb1618c7c04d8271593b55
psegynola <philipsegynola@gmail.com>
2026-08-05 20:53:51 -0600

    added backend resources to terraform

---
+++ infrastructure/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "fdivine-recipe-app-tf-state"
+    key            = "daily-task-planner/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "fdivine-recipe-api-tf-lock"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Project = var.project
+      Contact = var.contact
+    }
+  }
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  description = "The name of the project."
+  default     = "daily-task-planner-app"
+}
+
+variable "contact" {
+  description = "The contact information for the project."
+  default     = "philipsegynola@gmail.com"
+}
+
+variable "prefix" {
+  description = "The prefix to use for resource names."
+  default     = "dtp"
+}
