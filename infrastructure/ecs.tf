2aa89da6db1889c4d92487fdaa48440c7357707a
psegynola <philipsegynola@gmail.com>
2026-08-10 14:58:49 -0600

    fixed failing alb config

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -121,9 +121,9 @@ resource "aws_security_group" "ecs_service" {
 
   # HTTP inbound access
   ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
+    from_port       = 8000
+    to_port         = 8000
+    protocol        = "tcp"
     security_groups = [aws_security_group.lb.id]
   }
 }
@@ -148,7 +148,7 @@ resource "aws_ecs_service" "api" {
     security_groups = [aws_security_group.ecs_service.id]
   }
 
-    load_balancer {
+  load_balancer {
     target_group_arn = aws_lb_target_group.api.arn
     container_name   = "api"
     container_port   = 8000
--- infrastructure/load_balancer.tf
+++ infrastructure/load_balancer.tf
@@ -54,7 +54,7 @@ resource "aws_lb_listener" "api" {
   protocol          = "HTTP"
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.api.arn
   }
 }
